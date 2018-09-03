72442502d41cc353c76b2935cd8a86dee05ff8ef
Fei Gao <fgao1@ualberta.ca>
2018-09-03 15:48:44 -0600

    step 7 Implement downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var secrets = require('./secrets');
 var token = secrets['GITHUB_TOKEN'];
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   
@@ -19,12 +20,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
       });
   }
   
+function downloadImageByURL(url, filePath){
+    request.get(url)
+    .on('error', function(err){
+        throw err;
+    })
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    // console.log("Result:", result);
-    var keys = Object.keys(result);
-    for(var i = 0; i < keys.length; i++){
-        console.log(result[keys[i]]['avatar_url']);
-    }
-  });
+    .pipe(fs.createWriteStream(filePath))
+
+    .on('finish', function(){
+        console.log('write successfully!')
+    });
+
+}
+
+//   getRepoContributors("jquery", "jquery", function(err, result) {
+//     console.log("Errors:", err);
+//     // console.log("Result:", result);
+//     var keys = Object.keys(result);
+//     for(var i = 0; i < keys.length; i++){
+//         console.log(result[keys[i]]['avatar_url']);
+//     }
+//   });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
